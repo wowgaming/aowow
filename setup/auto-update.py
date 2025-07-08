aa454a26a3051d8cabfc21504f796060e1df6343
Stefano Borzì <stefanoborzi32@gmail.com>
2025-07-08 21:20:18 +0200

    chore(auto-update.py): add text for debugging ci

--- setup/auto-update.py
+++ setup/auto-update.py
@@ -42,10 +42,13 @@ def wait_until(value, byval=By.ID) -> None:
     except TimeoutException:
         print("error connection")
 
+print("Starting chrome")
 driver = webdriver.Chrome(options=chrome_options)
 
+print("Visiting altervista")
 driver.get("https://aa.altervista.org/index.php?client_id=altervista&response_type=code&lang=it&redirect_uri=http%3A%2F%2Fit.altervista.org%2Fcplogin.php")
 
+print("Login")
 wait_until("username", By.NAME)
 wait_until("password", By.NAME)
 wait_until("button", By.TAG_NAME)
@@ -54,8 +57,11 @@ sleep(1)
 driver.find_element(By.NAME, "username").send_keys("wowgaming")
 driver.find_element(By.NAME, "password").send_keys(password)
 driver.find_element(By.TAG_NAME, "button").click()
+print("submit credentials")
 
 sleep(5)
+
+print("Go to resources")
 a_elements = driver.find_elements(By.TAG_NAME, "a")
 for a in a_elements:
     if a.get_attribute("href") and a.get_attribute("href").find("db.pl?sid=") > -1:
@@ -64,26 +70,39 @@ for a in a_elements:
 
 
 sleep(5)
+print("Go to tools/backup")
 a_elements = driver.find_elements(By.TAG_NAME, "a")
 for a in a_elements:
     if a.get_attribute("href") and a.get_attribute("href").find("tools/backup/mysql_dump.pl?sid=") > -1:
         a.click()
         break
 
+print("Wait until dump")
 wait_until("dump", By.NAME)
+
+print("Wait until start")
 wait_until("start")
 
+print("Uploading aowow_update.sql")
 driver.find_element(By.NAME, "dump").send_keys(os.getcwd()+"/aowow_update.sql.zip")
 sleep(3)
 driver.find_element(By.ID, "start").click()
+
+print("Waiting 300s")
 sleep(300)
+
 print("aowow_update.sql loaded!")
 
+print("Uploading acore_world.sql")
 driver.find_element(By.NAME, "dump").send_keys(os.getcwd()+"/acore_world.sql.zip")
 sleep(3)
 driver.find_element(By.ID, "start").click()
+
+print("Waiting 300s")
 sleep(300)
 print("acore_world.sql loaded!")
 
 driver.close()
 driver.quit()
+
+print("DONE!")
