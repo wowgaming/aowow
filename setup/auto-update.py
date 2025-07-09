0d3415f290669b6fa821ff497d48845282f059fb
Stefano Borzì <stefanoborzi32@gmail.com>
2025-07-09 21:53:05 +0200

    ci: wip

--- setup/auto-update.py
+++ setup/auto-update.py
@@ -57,34 +57,34 @@ driver = webdriver.Chrome(options=chrome_options)
 print("Visiting altervista")
 driver.get("https://aa.altervista.org/index.php?client_id=altervista&response_type=code&lang=it&redirect_uri=http%3A%2F%2Fit.altervista.org%2Fcplogin.php")
 
-# print("Login")
-# wait_until("username", By.NAME)
-# wait_until("password", By.NAME)
-# wait_until("button", By.TAG_NAME)
-# sleep(1)
+print("Login")
+wait_until("username", By.NAME)
+wait_until("password", By.NAME)
+wait_until("button", By.TAG_NAME)
+sleep(1)
 
-# driver.find_element(By.NAME, "username").send_keys("wowgaming")
-# driver.find_element(By.NAME, "password").send_keys(password)
-# driver.find_element(By.TAG_NAME, "button").click()
-# print("submit credentials")
+driver.find_element(By.NAME, "username").send_keys("wowgaming")
+driver.find_element(By.NAME, "password").send_keys(password)
+driver.find_element(By.TAG_NAME, "button").click()
+print("submit credentials")
 
-# sleep(5)
+sleep(5)
 
-# print("Go to resources")
-# a_elements = driver.find_elements(By.TAG_NAME, "a")
-# for a in a_elements:
-#     if a.get_attribute("href") and a.get_attribute("href").find("db.pl?sid=") > -1:
-#         a.click()
-#         break
+print("Go to resources")
+a_elements = driver.find_elements(By.TAG_NAME, "a")
+for a in a_elements:
+    if a.get_attribute("href") and a.get_attribute("href").find("db.pl?sid=") > -1:
+        a.click()
+        break
 
 
-# sleep(5)
-# print("Go to tools/backup")
-# a_elements = driver.find_elements(By.TAG_NAME, "a")
-# for a in a_elements:
-#     if a.get_attribute("href") and a.get_attribute("href").find("tools/backup/mysql_dump.pl?sid=") > -1:
-#         a.click()
-#         break
+sleep(5)
+print("Go to tools/backup")
+a_elements = driver.find_elements(By.TAG_NAME, "a")
+for a in a_elements:
+    if a.get_attribute("href") and a.get_attribute("href").find("tools/backup/mysql_dump.pl?sid=") > -1:
+        a.click()
+        break
 
 # print("Wait until dump")
 # wait_until("dump", By.NAME)
