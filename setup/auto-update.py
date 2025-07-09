783c31cb8cba0a481b591613d34f8836216d2f7e
Stefano Borzì <stefanoborzi32@gmail.com>
2025-07-09 21:40:27 +0200

    ci: wip

--- setup/auto-update.py
+++ setup/auto-update.py
@@ -2,17 +2,17 @@
 
 print("Starting auto-update.py")
 
-# from time import sleep
-# from selenium import webdriver
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.common.exceptions import TimeoutException
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as ec
-# from selenium.webdriver.common.by import By
-# import chromedriver_autoinstaller
-# from pyvirtualdisplay import Display
-# import os
-# import sys
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+import chromedriver_autoinstaller
+from pyvirtualdisplay import Display
+import os
+import sys
 
 # print("Starting virtual display")
 
